0081e17112b88274cfa833717336345c1b343bdc
HarshitMalik001 <122423686+HarshitMalik001@users.noreply.github.com>
2024-08-21 17:20:34 +0530

    Create test.c

---
+++ C/Creating_your_own_library/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "calculator.h"
+
+int main()
+{
+        int a = 10, b = 5;
+
+        printf("%d %d %d %d \n",add(a, b), subtract(a, b), multiply(a, b), divide(a, b));
+
+        return 0;
+}
